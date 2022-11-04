2b94e9316bff96c203f0dac50b03d6cbf3f7f395
Lentsu <lenni.toikkanen@gmail.com>
2022-11-04 09:40:46 +0200

    update on scull

--- scull/scull.c
+++ scull/scull.c
@@ -4,7 +4,8 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/types.h>	// For dev_t
-#include <linux/fs.h>		// For device number registeration 
+#include <linux/fs.h>		// For device number registeration
+#include <linux/cdev.h>		// For
 
 /*
  *	LOCAL INCLUDES
@@ -16,9 +17,12 @@
 */
 int scull_major = SCULL_MAJOR;
 int scull_minor = 0;
-dev_t dev;
+dev_t devn;
 int scull_nr_devs = SCULL_NR_DEVS;
 const char* scull_name = SCULL_NAME;
+
+// Scull device structure
+scull_dev* s_dev;
 
 static int __init scull_init(void) {
 	
@@ -28,44 +32,75 @@ static int __init scull_init(void) {
 	 */
 	
 	// Initialize device number struct
-	dev = 0;
+	devn = 0;
 
 	int reg_chrdev_res;	// Holds the return value of device number registeration
 
 	// If major not set, allocate both numbers
 	if ( scull_major ) {
-		dev = MKDEV( scull_major, scull_minor );
-		reg_chrdev_res = register_chrdev_region( dev, scull_nr_devs, scull_name);
+		devn = MKDEV( scull_major, scull_minor );
+		reg_chrdev_res = register_chrdev_region( devn, scull_nr_devs, scull_name);
 	} else {
-		reg_chrdev_res = alloc_chrdev_region( &dev, scull_minor,
+		reg_chrdev_res = alloc_chrdev_region( &devn, scull_minor,
 											scull_nr_devs, scull_name );
 	}
-	
-	// Get major
-	scull_major = MAJOR(dev);
 	
 	/*	
 	 *	CHECK THAT ALL OPERATIONS WERE SUCCESSFULL	
 	 */
+	scull_major = MAJOR(devn);	// Get major
 	if (reg_chrdev_res) {
 		printk(KERN_ALERT "scull: can't get major %d\n", scull_major);
 		return reg_chrdev_res;
 	}
 
+	/*
+	 * ALLOCATE CDEV STRUCTURE
+	 */
+	struct cdev* s_dev->cdev = cdev_alloc();
+	
+	/*
+	 * SETUP THE SCULL DEVICE STRUCTURE
+	 */
+	scull_setup_cdev();
+
+	/*
+	 * NO INIT ERRORS
+	 */
 	return 0;
 }
 
 /* 
- * FUNCTION TO RETURNED THE USED RESOURCES.
+ * FUNCTION TO RETURN THE USED RESOURCES.
  * 	(UNLOADS EVERYTHING IN REVERSE ORDER)
  */
+
 static void __exit scull_exit(void) {
+	
+	// unregister character device structure <
+	cdev_del(&scull_dev->cdev);
+	// >
+
 	// unregister device numbers <
 	printk(KERN_ALERT "scull: trying to unregister chrdev '%s' with major %d\n",
 						scull_name, scull_major);
 	unregister_chrdev( scull_major, scull_name );
 	// >
 }
+
+/*
+ * SCULL FILE OPERATIONS STRUCTURE (DECLARED IN HEADER)
+ */
+scull_fops = {
+
+	.owner		= THIS_MODULE,
+	.llseek		= scull_llseek,
+	.read		= scull_read,
+	.write		= scull_write,
+	.ioctl		= scull_ioctl,
+	.open		= scull_open,
+	.release	= scull_release
+};
 
 // Init and exit as declared in linux/init.h
 module_init(scull_init);
--- scull/scull.h
+++ scull/scull.h
@@ -9,4 +9,22 @@
 static int __init scull_init(void);
 static void __exit scull_exit(void);
 
+struct scull_dev {
+
+	struct scull_qset *data;	/*	POINTER TO THE FIRST QUANTUM SET	*/
+	int quantum;				/*	THE CURRENT QUANTUM SIZE 			*/
+	int qset;					/*	THE CURRENT ARRAY SIZE 				*/
+	unsigned long size;			/*	AMOUNT OF DATA STORED HERE			*/
+	unsigned int access_key;	/*	USED BY SCULLUID AND SCULLPRIV		*/
+	struct semaphore sem;		/*	MUTUAL EXCLUSION SEMAPHORE			*/
+	struct cdev cdev;			/*	CHARACTER DEVICE					*/
+
+}; 
+
+// A method to setup scull character device structure
+static void scull_setup_cdev(struct scull_dev*, dev_t, int);
+
+// A structure for scull file operations
+struct file_operations scull_fops;
+
 #endif
